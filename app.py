83dff3ff1f93f47d3b31b93d4e1bc3607da8af42
Awrfik-Ghost <awrfikghost@gmail.com>
2024-10-13 20:10:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,39 +20,37 @@ def main():
     service = build('drive', 'v3', credentials=creds)
 
         
-    if st.button("List Files"):
-        # Specify the directory path
-        directory_path = '/mount/src/streamlitsqlitegdrivedev/'
-        files = list_files_in_directory(directory_path)
-        # Display the file information in Streamlit
-        if files:
-            st.write("Files in Directory:")
-            for file in files:
-                st.write(f"File Name: {file['file_name']}, File ID: {file['file_id']}, Last Modified: {file['last_modified']}")
-        else:
-            st.write("No files found in the specified directory.")
-        list_files(service)    
+    # if st.button("List Files"):
+    #     # Specify the directory path
+    #     directory_path = '/mount/src/streamlitsqlitegdrivedev/'
+    #     files = list_files_in_directory(directory_path)
+    #     # Display the file information in Streamlit
+    #     if files:
+    #         st.write("Files in Directory:")
+    #         for file in files:
+    #             st.write(f"File Name: {file['file_name']}, File ID: {file['file_id']}, Last Modified: {file['last_modified']}")
+    #     else:
+    #         st.write("No files found in the specified directory.")
+    #     list_files(service)    
     
     
     gdrive_modified_time = get_google_drive_modified_time(service, FILE_ID)
-    st.write(f"Google Drive file last modified: {gdrive_modified_time.strftime('%Y-%m-%d %H:%M:%S')}")
+    # st.write(f"Google Drive file last modified: {gdrive_modified_time.strftime('%Y-%m-%d %H:%M:%S')}")
 
     # Get the last modified time of the local database file
     local_modified_time = get_local_file_modified_time(DB_NAME)
     
     
     if local_modified_time:
-        st.write(f"Local file last modified: {local_modified_time.strftime('%Y-%m-%d %H:%M:%S')}")
+        # st.write(f"Local file last modified: {local_modified_time.strftime('%Y-%m-%d %H:%M:%S')}")
     else:
-        st.write("Local file does not exist.")
+        # st.write("Local file does not exist.")
     
     
     # Compare the modification times and download if Google Drive file is newer
     if not local_modified_time or gdrive_modified_time > local_modified_time:
-        st.write("Google Drive file is newer, downloading the latest file...")
+        # st.write("Google Drive file is newer, downloading the latest file...")
         download_db_from_drive(service, FILE_ID, DB_NAME)
-    else:
-        st.write("Local file is up to date.") 
 
     project_query = "SELECT project_id || ' - ' || project_name AS project FROM projects;"
     project = fetch_data_from_db(DB_NAME, project_query)
